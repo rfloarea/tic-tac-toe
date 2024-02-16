9515d5f0b9de331e90a1d990bdc761e0973cb0e6
rfloarea <account@rfloarea.net>
2024-02-15 21:58:39 -0500

    okay, nooow we have the handlers

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ const cell7 = gameboardUI.querySelector('#cell7');
 const cell8 = gameboardUI.querySelector('#cell8');
 const cell9 = gameboardUI.querySelector('#cell9');
 
+cell1.addEventListener("click", )
+cell2.addEventListener("click", )
+cell3.addEventListener("click", )
+cell4.addEventListener("click", )
+cell5.addEventListener("click", )
+cell6.addEventListener("click", )
+cell7.addEventListener("click", )
+cell8.addEventListener("click", )
+cell9.addEventListener("click", )
+
 
 const Cell = (function() {
     let value = 0;
